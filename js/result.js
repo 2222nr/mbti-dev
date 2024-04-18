4650e1b1a161b6c6e4aa8750f9e4eab421269488
2222nr <230320.nr.lee@gmail.com>
2024-04-18 15:15:25 +0900

    #10. 결과 페이지 완료

---
+++ js/result.js
@@ -0,0 +1,30 @@
+import { results, mbtis } from "./data.js";
+// http://127.0.0.1:5500/result.html?mbti=istj
+// mbti=istj! 
+const mbti = new URLSearchParams(location.search).get("mbti")
+// console.log('mbti = ' + mbti)
+
+const result = results[mbtis[mbti]]
+// console.log(result)
+
+const titleEl = document.querySelector(".page-title")
+const characterEl = document.querySelector(".character")
+const boxEls = document.querySelectorAll(".box")
+const jobsEls = document.querySelectorAll(".job")
+const lectureEl = document.querySelector(".lecture")
+const lectureImgEl = document.querySelector(".lecture img")
+
+titleEl.innerHTML = result.title
+characterEl.src = result.character
+
+boxEls.forEach(function(boxEl, index){
+  boxEl.innerHTML = result.results[index]
+})
+
+jobsEls.forEach(function(jobsEl, index){
+  jobsEl.innerHTML = result.jobs[index]
+})
+
+// 이미지 URL
+lectureEl.href = result.lectureUrl
+lectureImgEl.src = result.lectureImg
